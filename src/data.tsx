867a960e83bf5c9afc598ebd9b8230c19eb0cbd3
akashpathak9587 <aakapa95@gmail.com>
2024-04-28 10:31:18 +0530

    birthday reminder using react with updated frontend

---
+++ src/data.tsx
@@ -0,0 +1,163 @@
+// birthday json data sample
+export default [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    birthdate: "1995-04-02",
+  },
+  {
+    id: 2,
+    name: "Lila-Rose Humphreys",
+    age: 25,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959122/person-1_aufeoq.jpg",
+    birthdate: "1999-09-12",
+  },
+  {
+    id: 3,
+    name: "Quinton Humphreys",
+    age: 33,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-4_t9nxjt.jpg",
+    birthdate: "1991-12-05",
+  },
+  {
+    id: 4,
+    name: "Esmé Fitzgerald",
+    age: 22,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959125/person-3_rxtqvi.jpg",
+    birthdate: "2002-06-20",
+  },
+  {
+    id: 5,
+    name: "Soren Martinez",
+    age: 30,
+    image:
+      "https://images.unsplash.com/photo-1464863979621-258859e62245?q=80&w=2786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1994-10-08",
+  },
+  {
+    id: 6,
+    name: "Zara Mccoy",
+    age: 28,
+    image:
+      "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1996-03-15",
+  },
+  {
+    id: 7,
+    name: "Imran Holt",
+    age: 31,
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1993-07-29",
+  },
+  {
+    id: 8,
+    name: "Annika Rose",
+    age: 27,
+    image:
+      "https://images.unsplash.com/photo-1516726817505-f5ed825624d8?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1997-01-18",
+  },
+  {
+    id: 9,
+    name: "Tiana Shah",
+    age: 26,
+    image:
+      "https://images.unsplash.com/photo-1516195851888-6f1a981a862e?q=80&w=2805&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1998-11-23",
+  },
+  {
+    id: 10,
+    name: "Conrad Jennings",
+    age: 34,
+    image:
+      "https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1990-08-14",
+  },
+  {
+    id: 11,
+    name: "Aisling Wyatt",
+    age: 23,
+    image:
+      "https://images.unsplash.com/photo-1524701208119-e91fc2e4c8e5?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "2001-02-17",
+  },
+  {
+    id: 12,
+    name: "Anya Mccoy",
+    age: 32,
+    image:
+      "https://images.unsplash.com/photo-1530981785497-a62037228fe9?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1990-05-30",
+  },
+  {
+    id: 13,
+    name: "Kyran Nolan",
+    age: 29,
+    image:
+      "https://images.unsplash.com/photo-1608977144947-c11f1a75bdf7?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1995-08-09",
+  },
+  {
+    id: 14,
+    name: "Keziah Nolan",
+    age: 24,
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "2000-03-26",
+  },
+  {
+    id: 15,
+    name: "Yazmin Hopkins",
+    age: 35,
+    image:
+      "https://images.unsplash.com/photo-1589302736650-4fa300e27dd2?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1989-12-11",
+  },
+  {
+    id: 16,
+    name: "Adnan Powers",
+    age: 21,
+    image:
+      "https://images.unsplash.com/photo-1505535762334-d060c7df86e9?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "2003-01-28",
+  },
+  {
+    id: 17,
+    name: "Zachary Shaw",
+    age: 30,
+    image:
+      "https://images.unsplash.com/photo-1594724201056-76945d547091?q=80&w=2745&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1994-06-13",
+  },
+  {
+    id: 18,
+    name: "Livia-Rose Finch",
+    age: 27,
+    image:
+      "https://images.unsplash.com/photo-1582890158937-c11bebdc387f?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1997-09-22",
+  },
+  {
+    id: 19,
+    name: "Jonah Green",
+    age: 33,
+    image:
+      "https://images.unsplash.com/photo-1585612155794-c83acbc99359?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1991-04-05",
+  },
+  {
+    id: 20,
+    name: "Mollie Carney",
+    age: 28,
+    image:
+      "https://images.unsplash.com/photo-1581044777550-4cfa60707c03?q=80&w=2786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    birthdate: "1996-11-15",
+  },
+];
